853c56ca546fa96c62d8ee6609a28efd8a501561
OmerAkgull <143122832+OmerAkgull@users.noreply.github.com>
2024-01-09 12:43:59 +0300

    Create Input and useStates for goals

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import { GlobalStyle } from "./globalStyles";
+import { useState } from "react";
 
 function App() {
-  return <>
-    <GlobalStyle/>
-  </>;
+  const [todoInput, setTodoInput] = useState("");
+  const [todos, setTodos] = useState([
+    "Proje üzerine çalış",
+    "TypeScript öğren",
+  ]);
+  const listItems = todos.map((goal, index) => <li key={index}>{goal}</li>);
+ 
+
+  function addToDo() {
+    setTodos(...todos, todoInput);
+  }
+
+  return (
+    <>
+      <GlobalStyle />
+      <form>
+        <label htmlFor="#todoInput">Hedef</label>
+        <input value={todoInput} onChange={setTodoInput} id="todoInput"></input>
+        <button onClick={addToDo}>Add</button>
+      </form>
+      <ul>{listItems}</ul>
+    </>
+  );
 }
 
 export default App;
